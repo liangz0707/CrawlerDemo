739279bbc92d6c366807d0bedf71f01003e6979a
liangz0707 <liangzh0707@gmail.com>
2016-10-21 16:08:43 +0800

    '添加了pymongo'

--- spycraw/spycraw/spiders/maincrawler.py
+++ spycraw/spycraw/spiders/maincrawler.py
@@ -1,14 +1,29 @@
 __author__ = 'liangz14'
 # s
 import scrapy
+from pymongo import MongoClient
 
 class Mycraw(scrapy.Spider):
     name='mycrawler'
     start_urls = [
         "http://www.qiushibaike.com/",
     ]
+    c = MongoClient()
 
     def parse(self, response):
-        from scrapy.shell import inspect_response
-        inspect_response(response,self)
+
         print response.xpath('//div[@class="content"]').extract();
+
+    def db_test(self):
+        #from scrapy.shell import inspect_response
+        #inspect_response(response,self)
+        # 获取数据库 C.db_name
+        # 获取表.C.db_name.collection_name;
+        db = self.c.test;
+        db.test.count*({'x':1})
+        result = db.test.insert_one({"x":1})
+        result.inserted_id
+        db.test.find_one({"x":1})
+
+        result = db.test.insert_many([{"x":1},{"x":2}])
+        result.inserted_ids
