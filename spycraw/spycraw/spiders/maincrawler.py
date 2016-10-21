7117fc66cea4e8450fbc9b96033a0a4a44814c0a
liangz0707 <liangzh0707@gmail.com>
2016-10-21 12:59:17 +0800

    '添加调试'

--- spycraw/spycraw/spiders/maincrawler.py
+++ spycraw/spycraw/spiders/maincrawler.py
@@ -1,5 +1,5 @@
 __author__ = 'liangz14'
-
+# s
 import scrapy
 
 class Mycraw(scrapy.Spider):
@@ -9,4 +9,6 @@ class Mycraw(scrapy.Spider):
     ]
 
     def parse(self, response):
+        from scrapy.shell import inspect_response
+        inspect_response(response,self)
         print response.xpath('//div[@class="content"]').extract();
